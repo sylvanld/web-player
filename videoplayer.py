8e04028065e02e9e28a3b45da9ab284f896b9d98
Sylvan LE DEUNFF <sylvan.ledeunff@orange.com>
2021-09-25 12:45:56 +0200

    Update videoplayer.py

--- videoplayer.py
+++ videoplayer.py
@@ -12,7 +12,7 @@ from flask import Flask, Response, redirect, render_template, request, url_for
 try:
     USERNAME = environ["USERNAME"]
     PASSWORD_HASH = environ["PASSWORD_HASH"]
-    VIDEO_SERVER_URL = environ["VIDEO_SERVER_URL"]
+    VIDEOS_SERVER_URL = environ["VIDEOS_SERVER_URL"]
 except KeyError as error:
     raise Exception("Missing the following environment variable: %s" % str(error))
 
@@ -50,7 +50,7 @@ def basicauth(func):
 
 def get_videos():
     try:
-        response = requests.get(VIDEO_SERVER_URL + "/manifest.json")
+        response = requests.get(VIDEOS_SERVER_URL + "/manifest.json")
         return response.json()["videos"]
     except (requests.exceptions.ConnectionError, JSONDecodeError):
         return []
@@ -60,7 +60,7 @@ def get_video(filename):
     videos = get_videos()
     try:
         video = next(video for video in videos if video["filename"] == filename)
-        video["absolute_url"] = VIDEO_SERVER_URL + "/" + filename
+        video["absolute_url"] = VIDEOS_SERVER_URL + "/" + filename
         return video
     except StopIteration:
         return None
