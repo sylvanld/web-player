e4d039fe669cd9b655ee07c2686fd472f03a7da3
Sylvan LE DEUNFF <sledeunf@gmail.com>
2021-09-25 11:11:05 +0200

    create simple video player

---
+++ README.md
@@ -0,0 +1,38 @@
+# Video Player
+
+*This is a minimalist service that provide a **web interface** to **play MKV videos** from an **FTP server***
+
+## Configuration
+
+When starting the container, you must provide the following environment variables
+* **USERNAME**: Name used to authenticate
+* **PASSWORD_HASH**: Hash of the password used to authenticate ([how do I get my password's hash ?](#hash_password))
+* **VIDEO_SERVER_URL**: Path to the folder where [videos manifest](#videos_manifest) is stored
+
+### Hash password
+
+Here is an example of how you can hash your password using python:
+
+```python
+import hashlib
+
+my_password = "toto"
+password_hash = hashlib.sha256(my_password.encode("utf-8")).hexdigest()
+print(password_hash)
+```
+
+### Videos manifest
+
+It is a simple JSON file that map a name to a file. E.G:
+This file must be included along with the videos at the root of your FTP server.
+
+```json
+{
+    "videos": [
+        {
+            "title": "My video",
+            "filename": "my_video.mkv"
+        }
+    ]
+}
+```
---
+++ videoplayer.py
@@ -0,0 +1,75 @@
+import hashlib
+import json
+from base64 import b64decode
+from functools import wraps
+from json.decoder import JSONDecodeError
+
+import requests
+from flask import Flask, Response, redirect, render_template, request, url_for
+
+from settings import PASSWORD_HASH, USERNAME, VIDEO_SERVER_URL
+
+
+def hash_password(plain_password: str) -> str:
+    return hashlib.sha256(plain_password.encode("utf-8")).hexdigest()
+
+
+def get_credentials():
+    authz_header = request.headers.get('Authorization')
+    if authz_header is None or not authz_header.startswith("Basic "):
+        return None
+    credentials = b64decode(authz_header[6:]).decode('utf-8').split(':')
+    return {"username": credentials[0], "password": credentials[1]}
+
+
+def basicauth(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        credentials = get_credentials()
+        if credentials and credentials["username"] == USERNAME and hash_password(credentials["password"]) == PASSWORD_HASH:
+            return func(*args, **kwargs)
+        return Response("Merci de vous authentifier", headers={
+            "WWW-Authenticate": "Basic realm=videoplayer"
+        }, status=401)
+    return wrapper
+
+
+def get_videos():
+    try:
+        response = requests.get(VIDEO_SERVER_URL + "/manifest.json")
+        return response.json()["videos"]
+    except (requests.exceptions.ConnectionError, JSONDecodeError):
+        return []
+
+
+def get_video(filename):
+    videos = get_videos()
+    try:
+        video = next(
+            video for video in videos if video["filename"] == filename)
+        video["absolute_url"] = VIDEO_SERVER_URL + "/" + filename
+        return video
+    except StopIteration:
+        return None
+
+
+app = Flask(__name__, static_folder="videos", static_url_path="/files/videos")
+
+
+@app.route('/')
+@basicauth
+def dashboard():
+    return redirect(url_for("browse_videos"))
+
+
+@app.route('/videos')
+@basicauth
+def browse_videos():
+    videos = get_videos()
+    return render_template("browse_videos.html", videos=videos)
+
+
+@app.route('/videos/<string:filename>')
+@basicauth
+def play_video(filename: str):
+    return render_template("play_video.html", video=get_video(filename))
